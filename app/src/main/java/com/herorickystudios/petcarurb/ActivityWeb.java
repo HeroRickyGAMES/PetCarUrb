efa746df655f8dea46d5ff68daa1c7b4bc4d09b6
HeroRickyGAMES <ricojn9@gmail.com>
2022-11-11 21:53:35 -0300

    Primeira commit

---
+++ app/src/main/java/com/herorickystudios/petcarurb/ActivityWeb.java
@@ -0,0 +1,56 @@
+package com.herorickystudios.petcarurb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+//Programado por HeroRicky_Games e Doménica
+
+public class ActivityWeb extends AppCompatActivity {
+
+
+    WebView webV;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web);
+
+        webV=(WebView)findViewById(R.id.webView);
+
+        //URL do site
+        String url= "petcarurbaomeupetc.wixsite.com/petcarurb";
+
+        //WebViewClient() para que se abra dentro de la aplicacion
+        webV.setWebViewClient(new WebViewClient());
+
+        //Iniciamos la busqueda
+        webV.loadUrl("https://"+url);
+        webV.getSettings().setJavaScriptEnabled(true);
+        webV.getSettings().setAllowContentAccess(true);
+
+        //Esconde a action Bar
+        getSupportActionBar().hide();
+
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_BACK:
+                    if (webV.canGoBack()) {
+                        webV.goBack();
+                    } else {
+                        finish();
+                    }
+                    return true;
+            }
+
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+}
